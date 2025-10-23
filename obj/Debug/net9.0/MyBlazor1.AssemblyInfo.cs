805f6a4fa2a2b4a6703f38777b41b2aa37e67698
hafidhuddien <m.hafid@ifca.co.id>
2025-10-23 17:17:41 +0700

    update invoice

--- obj/Debug/net9.0/MyBlazor1.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyBlazor1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlazor1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4ac7e6f41e3b0d536c12e7000b0e837052e157f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bb6b917a66a62cffbdc99fcbb7c1a53b12374b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlazor1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlazor1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
