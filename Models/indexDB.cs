f4ac7e6f41e3b0d536c12e7000b0e837052e157f
hafidhuddien <m.hafid@ifca.co.id>
2025-10-23 14:39:41 +0700

    add cash flow

---
+++ Models/indexDB.cs
@@ -0,0 +1,87 @@
+using Magic.IndexedDb;
+using Magic.IndexedDb.Interfaces;
+using Magic.IndexedDb.SchemaAnnotations;
+using System;
+
+public class IndexedDbContext : IMagicRepository
+{
+    public static readonly IndexedDbSet Client = new("Client");
+    public static readonly IndexedDbSet Employee = new("Employee");
+    public static readonly IndexedDbSet Animal = new("Animal");
+    public static readonly IndexedDbSet CashFlow = new("CashFlow");
+}
+
+
+
+public class Client : MagicTableTool<Client>, IMagicTable<IndexedDbContext>
+{
+    // Databases property returning the IndexedDbContext instance
+    public IndexedDbContext Databases { get; } = new IndexedDbContext();
+
+    public IMagicCompoundKey GetKeys() =>
+        CreatePrimaryKey(x => x.Id, true);
+
+    public string GetTableName() => "Client";
+
+    public IndexedDbSet GetDefaultDatabase() => IndexedDbContext.Client;
+
+    // Implement compound indexes if you have any, else null
+    public List<IMagicCompoundIndex> GetCompoundIndexes()
+    {
+        // Return an empty list if you don't have any compound indexes
+        return new List<IMagicCompoundIndex>();
+    }
+
+    // Your model properties
+    public int Id { get; set; }
+
+    [MagicIndex]
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+
+}
+
+
+public class CashFlow : MagicTableTool<CashFlow>, IMagicTable<IndexedDbContext>
+{
+    // // Databases property returning the IndexedDbContext instance
+    public IndexedDbContext Databases { get; } = new IndexedDbContext();
+
+    public IMagicCompoundKey GetKeys() =>
+        CreatePrimaryKey(x => x.Id, true);
+
+    public string GetTableName() => "CashFlow";
+
+    public IndexedDbSet GetDefaultDatabase() => IndexedDbContext.CashFlow;
+
+    // Implement compound indexes if you have any, else null
+    public List<IMagicCompoundIndex> GetCompoundIndexes()
+    {
+        // Return an empty list if you don't have any compound indexes
+        return new List<IMagicCompoundIndex>();
+    }
+
+    // Your model properties
+    public int Id { get; set; }
+    [MagicIndex]
+
+    // For dates, use DateTime type (not string)
+    public DateTime TransactionDate { get; set; } = DateTime.Now;
+
+    // Description as string (no length constraints here, usually enforced in DB)
+    public string? Description { get; set; }
+
+    // Decimal types for money amounts
+    public decimal CashInflow { get; set; } = 0;
+
+    public decimal CashOutflow { get; set; } = 0;
+
+    // Computed property (not mapped to DB by default, but you can calculate)
+    public decimal NetCashFlow => CashInflow - CashOutflow;
+
+    // EndingBalance is usually calculated elsewhere, but if stored:
+    public decimal EndingBalance { get; set; }
+
+
+}
--- obj/Debug/net9.0/MyBlazor1.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyBlazor1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlazor1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d671d627a80d9d8394f07937a2078c4fb6679014")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee43c416083fddb53f009679a2fadf2c5d0bf053")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlazor1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlazor1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
